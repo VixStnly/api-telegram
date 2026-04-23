e95bbbcb8eaa156281d18ae394195bd1e2081c5d
VixStnly <rivalditzy@gmail.com>
2026-04-24 00:58:57 +0700

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             ManagedDeviceSeeder::class,
+            AdminUserSeeder::class,
+
             AutoReplyRuleSeeder::class,
         ]);
       
